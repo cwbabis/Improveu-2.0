16a7e305ba23cdf416d700fce62ae98752da8d4d
robertmadril <robertmmadril@gmail.com>
2019-06-15 13:31:01 -0700

    created card component

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Wrapper from "./components/Wrapper";
+import Card from "./components/Card";
 
 class App extends Component {
   render() {
     return (
       <Wrapper>
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+        </div>
       </Wrapper>
     );
   }
