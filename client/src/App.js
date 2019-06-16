b16edb8ebbea104dbc845f092e4f3849bd7637ee
Babis <cwbabis@gmail.com>
2019-06-15 21:26:39 -0700

    nav bar update

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,10 @@ import Nav from "./components/Nav";
 class App extends Component {
   render() {
     return (
+      <div>
         <Nav />
+        <Card />
+      </div>
     );
   }
 }
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,14 +4,27 @@ import "./style.css";
 function Nav() {
     return (
         <nav className="navbar navbar-default">
-         {/*    <div className="user-name">
+            <div className="container">
+                {/*    <div classNameName="user-name">
                 <img id="userPhoto" />
                 <p id="local-name"></p>
             </div> */}
-            <div className="nav-title">
-                <h1>Improveu</h1>
+                <div class="nav-title">
+                    <h1>Improveu</h1>
+                </div>
+                <ul className="nav navbar-nav">
+                    <li className="nav-item nav-link">
+                        <i className="fa fa-home nav-link"><a className="nav-link" href="#">Home</a></i>
+                    </li>
+                    <li className="nav-item nav-link">
+                        <i className="fa fa-user nav-link"><a className="nav-link" href="#">Profile</a></i>
+                    </li>
+                    <li className="nav-item nav-link">
+                        <i className="fa fa-arrow-right nav-link"><a className=" nav-link" href="#">Sign out</a></i>
+                    </li>
+                </ul>
             </div>{/* 
-            <button id="target" type="button" className="fas fa-plus fa-2x btn btn-success btn-circle btn-lg"></button> */}
+            <button id="target" type="button" classNameName="fas fa-plus fa-2x btn btn-success btn-circle btn-lg"></button> */}
         </nav>
     )
 }
