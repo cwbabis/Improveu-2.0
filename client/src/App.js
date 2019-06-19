b0ad7fa581ef018c7a0e2ffe641bbf189039b15a
cwbabis <cwbabis@gmail.com>
2019-06-18 19:57:55 -0700

    fixed the file structure and added react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Landing from "./pages/Landing";
 import Profile from "./pages/Profile";
-import Nav from "./components/Nav";
 /* import Footer from "./components/Footer"; */
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav/>
-        
           <Route exact path="/" component={Landing} />
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/dashboard" component={Dashboard} />
-        
       {/*   <Footer /> */}
       </div>
     </Router>
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 /* import "./App.css"; */
-import GoalCard from "./components/GoalCard";
-import Nav from "./components/Nav";
-import UserCard from "./components/UserCard";
-import Row from "./components/Row";
+import GoalCard from "../../components/GoalCard";
+import Nav from "../../components/Nav";
+import UserCard from "../../components/UserCard";
+import Row from "../../components/Row";
 /* import Col from "./components/Col"; */
 
 class Dashboard extends Component {
