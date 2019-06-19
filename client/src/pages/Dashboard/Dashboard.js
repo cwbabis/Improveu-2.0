295e699edc06fb2cab9e831421a5d6409738c810
cwbabis <cwbabis@gmail.com>
2019-06-18 19:42:17 -0700

    fixing file structure

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
+/* import "./App.css"; */
+import GoalCard from "./components/GoalCard";
+import Nav from "./components/Nav";
+import UserCard from "./components/UserCard";
+import Row from "./components/Row";
+/* import Col from "./components/Col"; */
 
 class Dashboard extends Component {
-};
+  render() {
+    return (
+      <div>
+        <Nav />
+        <br></br><br></br>
+        <Row>
+          <UserCard />
+          <GoalCard />
+        </Row>
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
