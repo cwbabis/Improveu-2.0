7e360244c8ac7fe414625b69d27d13149e23c796
Tristan <magombotristan@gmail.com>
2019-06-18 19:37:25 -0700

    Added passport.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
         </p>
       </div>
       </Wrapper>
+      
     );
   }
 }
--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser")
+const passport = require('passport');
 const PORT = process.env.PORT || 3001;
+const expressSession = require('express-session');
 const app = express();
+const mongoose = require("mongoose");
 
-// Define middleware here
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 }
-
-// Define API routes here
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/improveu");
+const connection = mongoose.connection;
+connection.once('open', function () {
+  console.log("MongoDB database connection established successfully");
+})
+// API routes
 
 // Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, () => {
