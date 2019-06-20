e02165e1b0bed4ddc7b8a7e74bccfa90509df145
Babis <cwbabis@gmail.com>
2019-06-19 21:38:14 -0700

    Dashboard Styling

--- client/src/components/GoalCard/index.js
+++ client/src/components/GoalCard/index.js
@@ -7,25 +7,34 @@ function GoalCard(props) {
             <div className="card">
                 <div className="card-header">
                     Example goal title
-            <div className="float-right text-muted">
+                    <div className="float-right text-muted">
                         Buddy Status:
-                </div>
+                    </div>
                 </div>
                 <div>
-                    <img src="https://via.placeholder.com/50" alt="placeholder"></img>
-                    <p>Example goal body</p>
+                    <div>
+                        <a href="#"><img class="goalcardpropic " src="https://via.placeholder.com/60" alt="placeholder"></img></a>
+                        <div>
+                            <a href="#" ><p class="goalCardUsername">Username Placeholder</p></a>
+                            <time className="float-left timeago" dateTime="">
+                                Time
+                            </time>
+                        </div>
+
+                    </div>
                 </div>
-                <hr></hr>
-                <div className="card-footer text-muted float-left">
-                    <time className="float-left timeago" dateTime="">
-                        Time
-                </time>
+                <div class="card-body">
+                    <p class="goalText">Deserunt esse nisi velit tempor eiusmod. Dolore incididunt eiusmod dolore eu sint culpa ea esse fugiat nulla eu dolor. Lorem elit reprehenderit Lorem nostrud magna ullamco excepteur dolore esse pariatur eu velit. Commodo nostrud in irure magna qui ut proident enim laboris adipisicing enim nisi exercitation. Aliquip elit sunt sit ea tempor nisi Lorem.</p>
+                </div>
+
+                <div className="card-footer text-muted">
+
                     <button className="btn btn-success btn-sm float-right buddy-button" id="buddy-button">
                         Buddy Up
                 </button>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 
