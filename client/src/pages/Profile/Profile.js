d823e4621080dcee938d598f2863c853c04a7537
robertmadril <robertmmadril@gmail.com>
2019-06-20 16:47:57 -0700

    add about me component and updated profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,6 +1,50 @@
 import React, { Component } from "react";
+/* import "./App.css"; */
+import GoalCard from "../../components/GoalCard";
+import Nav from "../../components/Nav";
+import UserCard from "../../components/UserCard";
+import Row from "../../components/Row";
+import Achievement from "../../components/Achievement";
+import Col from "../../components/Col";
+import AboutMe from "../../components/AboutMe";
 
 class Profile extends Component {
-};
+  render() {
+    return (
+      <div>
+        <Nav />
+        <br></br><br></br>
+        <Row>
+        <Col size="md-3">
+          <Row>
+            <UserCard/>
+          </Row>
+          <Row>
+            <Achievement />
+          </Row>
+        </Col>
+        <Col size="md-9">
+          <Row>
+            <AboutMe />
+          </Row>
+          <Row className="col-md-9">
+          My Goals
+          </Row>
+          <Row className="col-md-9" >
+            <GoalCard />
+          </Row>
+          <Row className="col-md-9" >
+            <GoalCard />
+          </Row>
+          <Row className="col-md-9" >
+            <GoalCard />
+          </Row>
+        </Col>
+
+        </Row>
+      </div>
+    );
+  }
+}
 
 export default Profile;
