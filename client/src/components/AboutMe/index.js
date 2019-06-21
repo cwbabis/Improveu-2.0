2a8500a5f56ff2b4aa1eccb1508b6ae6400ef2b4
robertmadril <robertmmadril@gmail.com>
2019-06-20 20:05:38 -0700

    finished up profile page top section

--- client/src/components/AboutMe/index.js
+++ client/src/components/AboutMe/index.js
@@ -1,19 +1,20 @@
 import React from "react";
-/* import "./index.css"; */
+import "./about.css";
 
 function AboutMe(props) {
     return (
-        <div className="col-md-12">
-            <div className="card">
+        <div className="col-md-12 h-100">
+            <div className="card h-100">
                 <div className="card-header">
                     About Me
                 </div>
                 <div className="card-body">
                     <p className="bioText">Deserunt esse nisi velit tempor eiusmod. Dolore incididunt eiusmod dolore eu sint culpa ea esse fugiat nulla eu dolor. Lorem elit reprehenderit Lorem nostrud magna ullamco excepteur dolore esse pariatur eu velit. Commodo nostrud in irure magna qui ut proident enim laboris adipisicing enim nisi exercitation. Aliquip elit sunt sit ea tempor nisi Lorem.</p>
-                    
+
                     <p className="bioText">Deserunt esse nisi velit tempor eiusmod. Dolore incididunt eiusmod dolore eu sint culpa ea esse fugiat nulla eu dolor. Lorem elit reprehenderit Lorem nostrud magna ullamco excepteur dolore esse pariatur eu velit. Commodo nostrud in irure magna qui ut proident enim laboris adipisicing enim nisi exercitation. Aliquip elit sunt sit ea tempor nisi Lorem.</p>
                     
-                    <p className="bioText">Deserunt esse nisi velit tempor eiusmod. Dolore incididunt eiusmod dolore eu sint culpa ea esse fugiat nulla eu dolor. Lorem elit reprehenderit Lorem nostrud magna ullamco excepteur dolore esse pariatur eu velit. Commodo nostrud in irure magna qui ut proident enim laboris adipisicing enim nisi exercitation. Aliquip elit sunt sit ea tempor nisi Lorem.</p>
+                    <p className="bioText">Deserunt esse nisi velit tempor eiusmod. Dolore incididunt eiusmod dolore eu sint culpa ea esse fugiat nulla eu dolor. Lorem elit reprehenderit Lorem nostrud magna ullamco excepteur dolore esse pariatur eu velit. Commodo nostrud in irure magna qui ut proident enim laboris adipisicing enim nisi exercitation. Aliquip elit sunt sit ea tempor nisi Lorem.</p>                    
+    
                 </div>
 
                 <div className="card-footer text-muted">
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,50 +1,65 @@
 import React, { Component } from "react";
-/* import "./App.css"; */
 import GoalCard from "../../components/GoalCard";
 import Nav from "../../components/Nav";
-import UserCard from "../../components/UserCard";
+import ProfileCard from "../../components/ProfileCard";
 import Row from "../../components/Row";
 import Achievement from "../../components/Achievement";
 import Col from "../../components/Col";
 import AboutMe from "../../components/AboutMe";
 
 class Profile extends Component {
-  render() {
-    return (
-      <div>
-        <Nav />
-        <br></br><br></br>
-        <Row>
-        <Col size="md-3">
-          <Row>
-            <UserCard/>
-          </Row>
-          <Row>
-            <Achievement />
-          </Row>
-        </Col>
-        <Col size="md-9">
-          <Row>
-            <AboutMe />
-          </Row>
-          <Row className="col-md-9">
-          My Goals
-          </Row>
-          <Row className="col-md-9" >
-            <GoalCard />
-          </Row>
-          <Row className="col-md-9" >
-            <GoalCard />
-          </Row>
-          <Row className="col-md-9" >
-            <GoalCard />
-          </Row>
-        </Col>
+    render() {
+        return (
+            <div>
+                <Nav />
+                <br></br><br></br>
+                <Row>
+                    <Col size="md-4">
+                        <Row>
+                            <ProfileCard />
+                        </Row>
+                        <Row>
+                            <Achievement />
+                        </Row>
+                    </Col>
+                    <div className="col-md-8">
+                        <div className="h-100">
+                                <AboutMe />
+                        </div>
+                    </div>
+                </Row>
+                <br></br><br></br><br></br>
+                <Row>
+                    <Row>
+                        <h1 className="text-center">My Goals</h1>
+                    </Row>
+                    <Col size="md-6">
+                        <Row>
+                            <GoalCard />
+                        </Row>
+                        <Row >
+                            <GoalCard />
+                        </Row>
+                        <Row >
+                            <GoalCard />
+                        </Row>
+                    </Col>
+                    <Col size="md-6">
+                        <Row>
+                            <GoalCard />
+                        </Row>
+                        <Row >
+                            <GoalCard />
+                        </Row>
+                        <Row >
+                            <GoalCard />
+                        </Row>
+                    </Col>
 
-        </Row>
-      </div>
-    );
-  }
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default Profile;
