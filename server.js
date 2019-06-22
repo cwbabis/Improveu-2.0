bb5bd475145439e1b7d2ec2556ee9e7b3cd01097
Mathew <maddaugh1@outlook.com>
2019-06-22 11:46:04 -0700

    image uploader

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const expressSession = require('express-session');
 const app = express();
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const multer = require("multer")
+const multer = require("multer");
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+Grid.mongo = mongoose.mongo;
+const gfs = Grid(connection.db)
 
-app.use(expressSession({secret: 'mySecretKey'}));
+app.use(expressSession({ secret: 'mySecretKey' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -17,11 +21,67 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use(multer({dest: "./uploads",
-rename: function (fieldname, filename){
-  return filename
-},
-}));
+var storage = GridFsStorage({
+  gfs : gfs,
+  filename: function (req, file, cb) {
+    var datetimestamp = Date.now();
+    cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
+  },
+  /** With gridfs we can store aditional meta-data along with the file */
+  metadata: function(req, file, cb) {
+    cb(null, { originalname: file.originalname });
+  },
+  root: 'ctFiles' //root name for collection to store files into
+});
+var storage = GridFsStorage({
+  gfs : gfs,
+  filename: function (req, file, cb) {
+    var datetimestamp = Date.now();
+    cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
+  },
+  /** With gridfs we can store aditional meta-data along with the file */
+  metadata: function(req, file, cb) {
+    cb(null, { originalname: file.originalname });
+  },
+  root: 'ctFiles' //root name for collection to store files into
+});
+
+app.get('/file/:filename', function(req, res){
+  gfs.collection('ctFiles'); //set collection name to lookup into
+
+  /** First check if file exists */
+  gfs.files.find({filename: req.params.filename}).toArray(function(err, files){
+    if(!files || files.length === 0){
+      return res.status(404).json({
+        responseCode: 1,
+        responseMessage: "error"
+      });
+    }
+    /** create read stream */
+    var readstream = gfs.createReadStream({
+      filename: files[0].filename,
+      root: "ctFiles"
+    });
+    /** set the proper content type */
+    res.set('Content-Type', files[0].contentType)
+    /** return response */
+    return readstream.pipe(res);
+  });
+});
+
+app.get('/file', function(req, res){
+  gfs.collection('ctFiles'); //set collection name to lookup into
+
+  gfs.files.find().toArray(function(err, files){
+    if(!files || files.length === 0){
+      return res.status(404).json({
+        responseCode: 1,
+        responseMessage: "error"
+      });
+    }
+    res.send(JSON.stringify(files));
+  });
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
