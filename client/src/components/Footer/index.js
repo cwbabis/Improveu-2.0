d5cfb0e7f5b85f57da1232212883218f5adb2b4c
cwbabis <cwbabis@gmail.com>
2019-06-29 13:44:32 -0700

    landing page styling

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css";
+
+function Footer(props) {
+    return (
+        <div className="footer col-12">
+            <div className="container noPad">
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+            <a className="links"href="https://www.w3schools.com">Footer link</a>
+
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
