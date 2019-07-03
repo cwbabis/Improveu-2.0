975a02b2dd65d103d03831732470b62738a3e26e
cwbabis <cwbabis@gmail.com>
2019-07-02 20:23:47 -0700

    trying to add google adsense

--- client/src/components/AdSpace/index.js
+++ client/src/components/AdSpace/index.js
@@ -1,19 +1,25 @@
 import React from "react";
+import "./style.css";
 
 function AdSpace(props) {
     return (
         <div className="">
             <div className="card">
-
-                Aute in proident excepteur ea do fugiat proident dolor. Enim veniam est dolore dolore do deserunt culpa quis voluptate sint. Minim nostrud irure sint id. Proident excepteur sint incididunt exercitation magna et aliqua consectetur pariatur labore tempor magna reprehenderit.
-
-Eiusmod ad enim nostrud qui proident. Cillum id laboris ex ex tempor fugiat non sit sit consequat enim duis Lorem aute. Amet deserunt sit veniam anim elit pariatur Lorem in culpa non cupidatat consectetur velit enim. In nostrud consequat deserunt cillum cillum cupidatat.
-
-Nostrud commodo ut aliqua et laboris magna magna cupidatat esse occaecat. Deserunt amet est tempor qui. Sunt qui est ea irure aliquip deserunt nisi dolore qui mollit eiusmod Lorem. Culpa laboris aliquip consequat est occaecat id id dolore labore nulla aliquip dolore. Ullamco minim Lorem cillum excepteur. Anim ut quis ea veniam tempor proident aliquip aliquip ipsum deserunt ea excepteur.
-iquip. Esse anim eu aliqua deserunt in voluptate eu nostrud ad exercitation. Cupidatat qui quis cillum officia. Irure sunt excepteur reprehenderit eu ut adipisicing. In ullamco amet anim nulla labore ad aliqua veniam.
-
-Cupidatat laborum cillum irure eiusmod ut officia adipisicing. Incididunt pariatur proident exercitation commodo deserunt. Voluptate mollit amet est nisi ad. Cupidatat eu ex ad enim Lorem adipisicing excepteur nulla nostrud minim veniam irure consequat deserunt.
+                <img className="fitbit" src="https://fortunedotcom.files.wordpress.com/2016/10/fitbit-flex-2_family-1.jpg"></img>
             </div>
+            <br></br>
+            <div className="card">
+                <img className="fitbit" src="https://fortunedotcom.files.wordpress.com/2016/10/fitbit-flex-2_family-1.jpg"></img>
+            </div>
+            <br></br>
+            <div className="card">
+                <img className="fitbit" src="https://fortunedotcom.files.wordpress.com/2016/10/fitbit-flex-2_family-1.jpg"></img>
+            </div>
+            <br></br>
+            <div className="card">
+                <img className="fitbit" src="https://fortunedotcom.files.wordpress.com/2016/10/fitbit-flex-2_family-1.jpg"></img>
+            </div>
+            <br></br>
         </div>
     )
 }
